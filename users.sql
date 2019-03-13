b5fb6dfe02372b788c8fb61b4dfc58d375e4a3fc
nuhundred@gmail.com <nuhundred@gmail.com>
2019-03-13 11:17:38 +0700

    edit users.sql

--- users.sql
+++ users.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 04, 2018 at 10:06 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `codecamp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+-- At database : 'sql_redis_test'
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -34,9 +9,6 @@ CREATE TABLE `users` (
   `money` int(11) NOT NULL DEFAULT '1000'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `salary`, `money`) VALUES
 (1, 'tester', 'Skywalker', 10000, 1000),
